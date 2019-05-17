63b3da784d1cec6969bfd98731b99791ca68917f
Ubuntu <ubuntu@ip-172-31-11-156.ec2.internal>
2019-05-17 04:29:36 +0000

    docker update

---
+++ src/index_bk.php
@@ -0,0 +1,17 @@
+<?php
+$conn = new mysqli("database", "root", ".sweetpwd.", "my_db");
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT name FROM users";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+        echo $row['name']."<br>";
+    }
+} else {
+    echo "Zero results";
+}
+$conn->close();
